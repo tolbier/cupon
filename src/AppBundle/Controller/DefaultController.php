5abb4245355b7a42f6c764daa29a1390b9ebccfa
toyos <toyos@kontiki.localdomain>
2017-01-25 13:18:48 +0100

    4.2.4.2 Valores por defecto (p57)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,9 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
 	/**
-	 * @Route("/sitio/{nombrePagina}")
+	 * @Route(	"/sitio/{nombrePagina}",
+	 * 			name="pagina"
+	 * )
 	 */
-	public function paginaAction($nombrePagina)
+	public function paginaAction($nombrePagina= 'ayuda')
 	{
 		return $this->render('sitio/'.$nombrePagina.'.html.twig');
 	}
