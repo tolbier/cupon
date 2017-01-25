2f36ea039ad9ef8e3eba741a490e7697d9f45533
toyos <toyos@kontiki.localdomain>
2017-01-25 12:30:52 +0100

    4.2.1 Respondiendo con plantillas (p51)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,14 +2,14 @@
 // src/AppBundle/Controller/DefaultController.php
 namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
-class DefaultController
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+class DefaultController extends Controller
 {
 	/**
 	 * @Route("/ayuda")
 	 */
 	public function ayudaAction()
 	{
-		return new Response('Página de ayuda');
+		return $this->render('sitio/ayuda.html.twig');
 	}
 }
