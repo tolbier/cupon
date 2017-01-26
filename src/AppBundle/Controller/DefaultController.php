b2a113058d3828079025f73a935951e73b8888da
toyos <toyos@kontiki.localdomain>
2017-01-26 18:02:44 +0100

    6.4 La plantilla (p105)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,19 +1,38 @@
 <?php
+
 // src/AppBundle/Controller/DefaultController.php
+
 namespace AppBundle\Controller;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-class DefaultController extends Controller
-{
-	/**
-	 * @Route(
-	 * "/sitio/{nombrePagina}/",
-	 * name="pagina",
-	 * requirements={ "nombrePagina"="ayuda|privacidad|sobre_nosotros" },
-	 * )
-	 */
-	public function paginaAction($nombrePagina= 'ayuda')
-	{
-		return $this->render('sitio/'.$nombrePagina.'.html.twig');
-	}
+
+class DefaultController extends Controller {
+
+    /**
+     * @Route("/", name="portada")
+     */
+    public function portadaAction() {
+        $em = $this->getDoctrine()->getManager();
+        $oferta = $em->getRepository('AppBundle:Oferta')->findOneBy(array(
+            'ciudad' => 1,
+            'fechaPublicacion' => (new \DateTime('today'))->setTime(23,59,59)
+        ));
+
+        return $this->render('portada.html.twig', array(
+                    'oferta' => $oferta
+        ));
+    }
+
+    /**
+     * @Route(
+     * "/sitio/{nombrePagina}/",
+     * name="pagina",
+     * requirements={ "nombrePagina"="ayuda|privacidad|sobre_nosotros" },
+     * )
+     */
+    public function paginaAction($nombrePagina = 'ayuda') {
+        return $this->render('sitio/' . $nombrePagina . '.html.twig');
+    }
+
 }
